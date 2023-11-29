f83c49169e91f7d32e49923d33a47f5902375487
kali xps13 <walter.djuric@gmail.com>
2023-11-29 21:26:56 +0100

    remap lsp signature_help shortcut keys

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,10 @@
+-- trying to remap lsp signature_help from C-k to C-o
+return {
+  "neovim/nvim-lspconfig",
+  init = function()
+    local keys = require("lazyvim.plugins.lsp.keymaps").get()
+    keys[#keys + 1] = { "<C-k>", mode = "i", false }
+    keys[#keys + 1] =
+      { "<C-o>", vim.lsp.buf.signature_help, mode = "i", desc = "Signature Help", has = "SignatureHelp" }
+  end,
+}
