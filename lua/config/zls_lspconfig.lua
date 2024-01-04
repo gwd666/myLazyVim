0cfa8ba39a4606361e37e47c1bb23151a6055cc3
ubuntu xps13 <walter.djuric@outlook.com>
2024-01-04 11:15:19 +0100

    fix typo

--- lua/config/zls_lspconfig.lua
+++ lua/config/zls_lspconfig.lua
@@ -2,7 +2,7 @@
 local lspconfig = require("lspconfig")
 
 local on_attach = function(_, bufnr)
-  vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:loa.vim.lsp.omnifunc")
+  vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
   require("completion").on_attach()
 end
 
