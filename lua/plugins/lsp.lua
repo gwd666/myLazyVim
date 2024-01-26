d7acd4a32f8c90fe0a49e2038c5b17ba265ea310
ubuntu xps13 <walter.djuric@outlook.com>
2024-01-27 00:06:16 +0100

    add Cmdline cmp-letion feature to lsp.lua

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -6,5 +6,29 @@ return {
     keys[#keys + 1] = { "<C-k>", mode = "i", false }
     keys[#keys + 1] =
       { "<C-o>", vim.lsp.buf.signature_help, mode = "i", desc = "Signature Help", has = "SignatureHelp" }
+    local cmp = require("cmp")
+
+    -- '/' cmdline setup
+    cmp.setup.cmdline("/", {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = {
+        { name = "buffer" },
+      },
+    })
+
+    -- ':' cmdline setup
+    cmp.setup.cmdline(":", {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({
+        { name = "path" },
+      }, {
+        {
+          name = "cmdline",
+          option = {
+            ignore_cmds = { "Man", "!" },
+          },
+        },
+      }),
+    })
   end,
 }
