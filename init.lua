c7170e14b3e31698e7242f4b5c38a9a5d6203cf9
gwd666 <walter.djuric@outlook.com>
2024-03-21 01:32:59 +0100

    deactivate init prints and keymaps for vs-code

--- init.lua
+++ init.lua
@@ -6,14 +6,14 @@
 
 if vim.g.vscode then
   -- VSC extension
-  print("Looks like you're starting Neovim from VSC!")
+  print("Looks like you're starting Neovim for VSC reading init from WSL!")
   -- local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-  -- require("config/keymaps")
   require("config/options") -- this makes H/L move to prev/next line etc
+  -- require("config/keymaps")
   -- require("")
   vim.cmd([[source /home/gwd/.config/nvim/vscode/vsc_settings.vim]])
 else
-  print("=== Sourcing " .. os.getenv("HOME") .. ".config/nvim/init.lua " .. " part =====")
+  -- print("=== Sourcing " .. os.getenv("HOME") .. ".config/nvim/init.lua " .. " part =====")
   -- I am gonna ride with SPACE as LEADER for now to test the feeling
   -- ----------------------------------------------------------------------------
   vim.g.mkdp_echo_preview_url = 1 -- default was 0 -> make MarkdownPreviewwe show the localhost URL for Peviewerh
@@ -28,17 +28,19 @@ else
   -- Start the bootstrap of lazy.nvim, LazyVim and your plugins
   -- bootstrap lazy.nvim, LazyVim and your plugins
   require("config.lazy")
+  -- load telescope config
   require("config.telescope")
   require("config.chatgpt")
   -- moved all the colorscheme stuff to plugins/colorscheme.lua
   -- define the colorscheme
-  vim.api.nvim_command([[ colorscheme catppuccin-frappe ]])
+  -- vim.api.nvim_command([[ colorscheme catppuccin-frappe ]])
   -- vim.api.nvim_command([[ colorscheme tokyonight-storm ]])
+  vim.api.nvim_command([[ colorscheme onenord ]])
   -- vim.api.nvim_command([[ colorscheme catppuccin-macchiato ]])
   -- telekasten setpu
   require("telekasten").setup({
     home = vim.fn.expand("~/zettelkasten"), -- Put the name of your notes directory here
   })
   ------------------------------------------------------------------------------
-  print("=== Finished " .. os.getenv("HOME") .. ".config/nvim/init.lua " .. " part =====")
+  -- print("=== Finished " .. os.getenv("HOME") .. ".config/nvim/init.lua " .. " part =====")
 end
