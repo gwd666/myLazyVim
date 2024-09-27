a1bd921293139e92689e3e734705abbbce610ea7
gwd666 <walter.djuric@outlook.com>
2024-09-27 22:20:56 +0200

    change new terminal keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -44,6 +44,9 @@ map("t", "<M-[>", [[<C-\><C-n>]], {})
 map("t", "<C-w>", [[<C-\><C-n><C-w>]], {})
 map("t", "<M-r>", [['<C-\><C-N>"'.nr2char(getchar()).'pi']], { expr = true })
 
+-- new terminal in normal mode below current window size 25
+map("n", "<leader>Tt", ":below 25sp term://zsh<CR>", { silent = true, desc = "New terminal below" })
+
 -- Keep matches center screen when cycling with n|N etc.
 map("n", "n", "nzzzv", { noremap = true, desc = "Fwd  search '/' or '?'" })
 map("n", "N", "Nzzzv", { noremap = true, desc = "Back search '/' or '?'" })
