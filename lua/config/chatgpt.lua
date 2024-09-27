2aae645c72f5ed0ee4adea2dda26a451084dab48
gwd666 <walter.djuric@outlook.com>
2024-09-27 22:23:55 +0200

    change model to gpt-4o, mod .gpg file path

--- lua/config/chatgpt.lua
+++ lua/config/chatgpt.lua
@@ -9,7 +9,7 @@ WELCOME_MESSAGE = [[
 
 require("chatgpt").setup({ -- for the below to work you need to run gpg -d secret.txt.gpg in terminal before starting nvim
   -- api_key_cmd = "gpg --decrypt C:\\Users\\gwd\\secret.txt.gpg",
-  api_key_cmd = "gpg --decrypt " .. home .. "\\secret.txt.gpg",
+  api_key_cmd = "gpg --decrypt " .. home .. "\\SER7_SHELL-GPT-OPENAI_API.gpg",
   edit_with_instructions = {
     keymaps = {
       toggle_help = "<M-h>", -- I changed it from <C-h> to <M-h> ie ALt+h like for other Telescope windows
@@ -23,9 +23,9 @@ require("chatgpt").setup({ -- for the below to work you need to run gpg -d secre
     },
   },
   openai_params = {
-    model = "gpt-4",
+    model = "gpt-4o",
     temperature = 0.1,
-    max_tokens = 150,
+    max_tokens = 300,
     frequency_penalty = 0,
     presence_penalty = 0,
     top_p = 1,
