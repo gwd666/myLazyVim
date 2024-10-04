ba74d118dd226c098359c8d808d91b34bc6ed7c1
gwd666 <walter.djuric@outlook.com>
2024-10-04 22:00:38 +0200

    mods and additions to lsp configurations

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -6,7 +6,7 @@ return {
     keys[#keys + 1] = { "<C-k>", mode = "i", false }
     keys[#keys + 1] =
       { "<C-o>", vim.lsp.buf.signature_help, mode = "i", desc = "Signature Help", has = "SignatureHelp" }
-    --  tyr to fix ""multiple different client offset_encodings detected" warning
+    --  tyr to fix ""multiple different client offset_encodings detected" warning for clangd lsp
     local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
     opts = { -- some lines to fix mason and lsp conflicts for rust-analyzer
@@ -17,6 +17,76 @@ return {
       },
     }
 
+    -- include folliwing lines to avoid scanning C:\Users\gwd ie the home dir
+    root_dir = function(fname)
+      local root_pattern = lspconfig.util.root_pattern(".git")(fname)
+      if fname == vim.loop.os_homedir() then
+        return nil
+      end
+      return root_pattern or fname
+    end
+
+    -- have a look at https://github.com/neovim/nvim-lspconfig/wiki/UI-Customization
+    -- for customization of the floating window borders etc.
+    vim.cmd([[autocmd! ColorScheme * highlight NormalFloat guibg=#1f2335]])
+    vim.cmd([[autocmd! ColorScheme * highlight FloatBorder guifg=grey guibg=#1f2335]])
+
+    -- To instead override globally
+    local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+    function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+      opts = opts or {}
+      opts.border = opts.border or border
+      return orig_util_open_floating_preview(contents, syntax, opts, ...)
+    end
+
+    local border = {
+      { "🭽", "FloatBorder" },
+      { "●", "FloatBorder" },
+      { "▔", "FloatBorder" },
+      { "🭾", "FloatBorder" },
+      { "●", "FloatBorder" },
+      { "▕", "FloatBorder" },
+      { "🭿", "FloatBorder" },
+      { "●", "FloatBorder" },
+      { "▁", "FloatBorder" },
+      { "🭼", "FloatBorder" },
+      { "●", "FloatBorder" },
+      { "▏", "FloatBorder" },
+    }
+
+    -- local border = {
+    --   { "🭽", "FloatBorder" },
+    --   { "▔", "FloatBorder" },
+    --   { "🭾", "FloatBorder" },
+    --   { "▕", "FloatBorder" },
+    --   { "🭿", "FloatBorder" },
+    --   { "▁", "FloatBorder" },
+    --   { "🭼", "FloatBorder" },
+    --   { "▏", "FloatBorder" },
+    -- }
+
+    -- Change border of documentation hover window, See https://github.com/neovim/neovim/pull/13998.
+    local lsp = vim.lsp
+    lsp.handlers["textDocument/hover"] = lsp.with(vim.lsp.handlers.hover, {
+      border = "rounded",
+    })
+
+    -- LSP settings (for overriding per client) -
+    -- start by defining a local handler table
+    local handlers = {
+      ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = border }),
+      ["textDocmumen/functionInfo"] = vim.lsp.with(vim.lsp.handlers.hover, { border = border }),
+      ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = border }),
+    }
+
+    lspconfig = require("lspconfig")
+
+    lspconfig.julials.setup({
+      on_attach = on_attach,
+      -- handlers = handlers,
+      root_dir = lspconfig.util.root_pattern("Project.toml", "JuliaProject.toml", ".git", vim.fn.getcwd()),
+    })
+
     require("lspconfig").clangd.setup({
       on_attach = on_attach,
       capabilities = cmp_nvim_lsp.default_capabilities(),
@@ -26,36 +96,38 @@ return {
       },
     })
 
-    vim.cmd([[autocmd! ColorScheme * highlight NormalFloat guibg=#1f2335]])
-    vim.cmd([[autocmd! ColorScheme * highlight FloatBorder guifg=grey guibg=#1f2335]])
+    lspconfig.gopls.setup({
+      on_attach = on_attach,
+      filetypes = { "go", "gomod" },
+      settings = {
+        gopls = {
+          analyses = {
+            unusedparams = true,
+          },
+          staticcheck = true,
+          gofumpt = true,
+        },
+      },
+      root_dir = lspconfig.util.root_pattern("go.mod", ".git", vim.fn.getcwd()),
+    })
 
-    local border = {
-      -- { "🭽", "FloatBorder" },
-      { "●", "FloatBorder" },
-      { "▔", "FloatBorder" },
-      -- { "🭾", "FloatBorder" },
-      { "●", "FloatBorder" },
-      { "▕", "FloatBorder" },
-      -- { "🭿", "FloatBorder" },
-      { "●", "FloatBorder" },
-      { "▁", "FloatBorder" },
-      -- { "🭼", "FloatBorder" },
-      { "●", "FloatBorder" },
-      { "▏", "FloatBorder" },
-    }
-
-    -- LSP settings (for overriding per client)
-    local handlers = {
-      ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = border }),
-      ["textDocmumen/functionInfo"] = vim.lsp.with(vim.lsp.handlers.hover, { border = border }),
-      ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = border }),
-    }
+    lspconfig.powershell_es.setup({
+      on_attach = on_attach,
+      filetypes = { "ps1", "psm1", "psd1" },
+      bundle_path = "~/.local/share/nvim/mason/packages/powershell-editor-services",
+      settings = { powershell = { codeFormatting = { Preset = "OTBS" } } },
+      init_options = {
+        enableProfileLoading = false,
+      },
+    })
 
     -- Do not forget to use the on_attach function
     require("lspconfig").zls.setup({
-      handlers = handlers,
+      on_attach = on_attach,
+      -- handlers = handlers,
       cmd = { "zls" },
       filetypes = { "zig", "zlr" },
+      root_dir = lspconfig.util.root_pattern(".git", "build.zig", vim.fn.getcwd()),
     }) -- this put HERE - made 'zls' workk!
 
     -- this commented out code below - breaks command completion in vim COMMAND mode/prompt
