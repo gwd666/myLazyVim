f1bbd39c3778f83cf78b71a35ccd89abf1c1cb48
gwd666 <walter.djuric@outlook.com>
2024-10-20 01:40:55 +0200

    reformat indents for slime ipython calling code

--- lua/plugins/quarto.lua
+++ lua/plugins/quarto.lua
@@ -73,12 +73,12 @@ return {
       vim.cmd([[
       let g:slime_dispatch_ipython_pause = 100
       function SlimeOverride_EscapeText_quarto(text)
-      call v:lua.Quarto_is_in_python_chunk()
-      if exists('g:slime_python_ipython') && len(split(a:text,"\n")) > 1 && b:quarto_is_python_chunk
-      return ["%cpaste -q\n", g:slime_dispatch_ipython_pause, a:text, "--", "\n"]
-      else
-      return a:text
-      end
+        call v:lua.Quarto_is_in_python_chunk()
+        if exists('g:slime_python_ipython') && len(split(a:text,"\n")) > 1 && b:quarto_is_python_chunk
+          return ["%cpaste -q\n", g:slime_dispatch_ipython_pause, a:text, "--", "\n"]
+        else
+          return a:text
+        end
       endfunction
       ]])
 
