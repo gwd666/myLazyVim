9d1367579d5dab0201778c6818707b44eeba4c0a
gwd666 <walter.djuric@outlook.com>
2025-10-24 16:42:51 +0200

    ci: -m making zellij-nav aware of ZELLIJ_SESSION and zellij executable

--- lua/config/zellij-nav.lua
+++ lua/config/zellij-nav.lua
@@ -1,6 +1,7 @@
-if vim.fn.executable("zellij") == 1 then
-  -- For lazy.nvim
-  require("lazy").setup({
-    { "willothy/nvim-zellij-nav" },
+if vim.fn.executable("zellij") == 1 and vim.env.ZELLIJ_SESSION_NAME then
+  -- NOTE: Ensures that when exiting NeoVim, Zellij returns to normal mode
+  vim.api.nvim_create_autocmd("VimLeave", {
+    pattern = "*",
+    command = "silent !zellij action switch-mode normal",
   })
 end
--- lua/plugins/zellij-nav.lua
+++ lua/plugins/zellij-nav.lua
@@ -1,12 +1,23 @@
+local function is_zellij_available_and_active()
+  local session = vim.env.ZELLIJ_SESSION_NAME
+  local zellij_exists = vim.fn.executable("zellij") == 1
+  local result = session and zellij_exists
+  return result
+end
+
 return {
   "swaits/zellij-nav.nvim",
   lazy = true,
+  cond = is_zellij_available_and_active(),
   event = "VeryLazy",
   keys = {
-    { "<c-h>", "<cmd>ZellijNavigateLeft<cr>", { silent = true, desc = "navigate left or tab" } },
-    { "<c-j>", "<cmd>ZellijNavigateDown<cr>", { silent = true, desc = "navigate down" } },
-    { "<c-k>", "<cmd>ZellijNavigateUp<cr>", { silent = true, desc = "navigate up" } },
-    { "<c-l>", "<cmd>ZellijNavigateRightTab<cr>", { silent = true, desc = "navigate right or tab" } },
+    { "<c-h>", "<cmd>ZellijNavigateLeft<cr>", mode = "n", silent = true, desc = "navigate left or tab" },
+    { "<c-j>", "<cmd>ZellijNavigateDown<cr>", mode = "n", silent = true, desc = "navigate down" },
+    { "<c-k>", "<cmd>ZellijNavigateUp<cr>", mode = "n", silent = true, desc = "navigate up" },
+    { "<c-l>", "<cmd>ZellijNavigateRightTab<cr>", mode = "n", silent = true, desc = "navigate right or tab" },
   },
-  opts = {},
+  config = function()
+    require("zellij-nav").setup()
+    require("config.zellij-nav")
+  end,
 }
