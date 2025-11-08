934dea8db46684295d74e822b97a3c1c345291f7
gwd666 <walter.djuric@outlook.com>
2025-11-08 19:30:13 +0100

    ci: -m adding vim-tmux-navigator to neovim b/c I rediscovered tmux f my workflow

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -13,4 +13,23 @@ return {
   { "juliaEditorSupport/julia-vim" },
   -- add zig.vim
   { "ziglang/zig.vim" },
+  {
+    "christoomey/vim-tmux-navigator",
+    cond = function()
+      return vim.fn.executable("tmux") == 1
+    end,
+    lazy = false,
+    keys = {
+      { "<C-h>", "<cmd>TmuxNavigateLeft<cr>", mode = "n", desc = "Go to Left Window/Pane" },
+      { "<C-j>", "<cmd>TmuxNavigateDown<cr>", mode = "n", desc = "Go to Lower Window/Pane" },
+      { "<C-k>", "<cmd>TmuxNavigateUp<cr>", mode = "n", desc = "Go to Upper Window/Pane" },
+      { "<C-l>", "<cmd>TmuxNavigateRight<cr>", mode = "n", desc = "Go to Right Window/Pane" },
+    },
+    init = function()
+      vim.g.tmux_navigator_save_on_switch = 0
+      vim.g.tmux_navigator_disable_when_zoomed = 0
+      vim.g.tmux_navigator_preserve_zoom = 0
+      vim.g.tmux_navigator_no_mappings = 1
+    end,
+  },
 }
